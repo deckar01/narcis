8a55667011c2820384bfba3fcab2783184b25970
Jared Deckard <jared.deckard@gmail.com>
2016-01-23 17:20:39 -0500

    Include project in path for wsgi

--- narcis/wsgi.py
+++ narcis/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "narcis.settings")
 
 from django.core.wsgi import get_wsgi_application
