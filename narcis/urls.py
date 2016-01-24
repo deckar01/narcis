31643caa2f87d51c3c3395d9d819e640637b3cef
Jared Deckard <jared.deckard@gmail.com>
2016-01-24 10:47:00 -0600

    Add placeholder URL

--- narcis/urls.py
+++ narcis/urls.py
@@ -4,11 +4,14 @@ from django.views.generic.base import RedirectView
 from django.contrib import admin
 admin.autodiscover()
 
+from . import views
+
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'narcis.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'narcis.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
+    url(r'^projects/', include('projects.urls')),
 )
