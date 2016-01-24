8524fb90a04c39bf4cfd7e6f4a7f0cbc5a04fdbf
Jared Deckard <jared.deckard@gmail.com>
2016-01-24 12:18:06 -0500

    Create the projects model

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Projects(models.Model):
+  name = models.CharField(max_length=200)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  creation_date = models.DateTimeField()
