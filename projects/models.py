c64159bb6330b3ef9a651e9ccf19da8883a738a5
Jared Deckard <jared.deckard@gmail.com>
2016-01-24 13:06:21 -0500

    Automatically set the time models are created

--- projects/models.py
+++ projects/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Projects(models.Model):
+class Project(models.Model):
   name = models.CharField(max_length=200)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
-  creation_date = models.DateTimeField()
+  creation_date = models.DateTimeField(auto_now_add=True)
