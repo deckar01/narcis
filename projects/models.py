693d61a5193d85098946189bc634a86e38f7318f
Jared Deckard <jared.deckard@gmail.com>
2016-01-24 13:25:38 -0500

    Add pages model

--- projects/models.py
+++ projects/models.py
@@ -8,3 +8,10 @@ class Project(models.Model):
   name = models.CharField(max_length=200)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   creation_date = models.DateTimeField(auto_now_add=True)
+
+# Create your models here.
+class Page(models.Model):
+  name = models.CharField(max_length=200)
+  url = models.CharField(max_length=2083)
+  project = models.ForeignKey(Project, on_delete=models.CASCADE)
+  creation_date = models.DateTimeField(auto_now_add=True)
