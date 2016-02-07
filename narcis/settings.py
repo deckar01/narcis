32228a723b965be3e9ea9ebaf4d998f168bc5d9a
Jared Deckard <jared.deckard@gmail.com>
2016-02-06 21:02:33 -0600

    Fix depricated TEMPLATE_DEBUG usage

--- narcis/settings.py
+++ narcis/settings.py
@@ -37,6 +37,27 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'debug': DEBUG,
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 ROOT_URLCONF = 'narcis.urls'
 
 WSGI_APPLICATION = 'narcis.wsgi.application'
